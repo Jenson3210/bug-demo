d51f28ba4b11e77481ece78499e2acfaeff0dff1
Jente Sondervorst <jente.sondervorst@colruytgroup.com>
2023-10-27 00:48:03 +0200

    initial commit

--- src/main/java/com/example/demo/ControllerExceptionHandler.java
+++ src/main/java/com/example/demo/ControllerExceptionHandler.java
@@ -4,4 +4,5 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
+//public class ControllerExceptionHandler {}
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {}
